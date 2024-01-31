2f9c8f1d4a82a7f54cda923927cfda8c20bc5799
mynametithi <127840793+mynametithi@users.noreply.github.com>
2024-01-31 17:56:21 +0530

    Add files via upload

---
+++ 5_First.c
@@ -0,0 +1,8 @@
+//Write a program to convert celcius to farenhite
+#include<stdio.h>
+int main(){
+float celcius=37,far;
+far=(celcius*9/5)+32;
+printf("The value of this celcius tempereture in Farenheit is %f",far);
+    return 0;
+}
