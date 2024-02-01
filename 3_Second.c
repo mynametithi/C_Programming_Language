fecb423b3b6ca221e8d41bb2c10035bfcc41f31c
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2024-02-01 13:44:56 +0530

    Add files via upload

---
+++ 3_Second.c
@@ -0,0 +1,14 @@
+//Arithmetic Operators 
+#include<stdio.h>
+int main(){
+int a=7;
+int b=9;
+int z;
+z=b*a;   //b*a=z (not allowed)
+printf("The value of z is : %d \n",z);
+printf("The value of a+b is : %d \n",a+b);
+printf("The value of a-b is : %d \n",a-b);
+printf("The value of a*b is : %d \n",a*b);
+printf("The value of a/b is : %d \n",a/b);
+    return 0;
+}
