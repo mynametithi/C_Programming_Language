7051c1d9341849895964fe0fba9948393be929dd
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2024-02-01 13:43:53 +0530

    Add files via upload

---
+++ 1_Second.c
@@ -0,0 +1,11 @@
+//Type declairation instruction part 01
+#include<stdio.h>
+int main(){
+//int a=4;
+int a=5,b,c;
+b=c=a;
+printf("The value of a is %d \n",a);
+printf("The value of b is %d \n",b);
+printf("The value of c is %d \n",c);
+    return 0;
+}
