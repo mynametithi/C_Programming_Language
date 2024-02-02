acfa0a397c37a8b7e3c8f549a59c754c060d24c8
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2024-02-02 10:29:43 +0530

    Add files via upload

---
+++ 5_Second.c
@@ -0,0 +1,12 @@
+//Operation Operators:
+#include<stdio.h>
+int main(){
+int x=2;
+int y=3;
+printf("The Value of 3x-8y is:%d /n",3*x-8*y);
+printf("The Value of 8*y / 3*x is:%d /n",8*y / 3*x);
+//8*y / 3*x=24/6 will give wrong answer
+//24/3*2
+//8*2=16 That is answer
+return 0;
+}  
