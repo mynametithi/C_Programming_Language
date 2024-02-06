1004a00f5b639657490aaa6e4a3edbd12b5966fb
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2024-02-06 09:36:42 +0530

    Add files via upload

---
+++ 7_Second _problem.c
@@ -0,0 +1,11 @@
+ //  1.Which of the following is invalid in C?
+//  (i) int a; b=a;
+//  (ii)int v=3^3
+//  (iii) char dt='21 dec 2020' 
+#include<stdio.h>
+int main(){
+int v=3^3;
+// char dt='2';
+printf("%d",v);
+    return 0;
+}
