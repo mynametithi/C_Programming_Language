72792308eed320fa214f8fbb4d1c64811e6aff3b
Tithi Saha <127840793+mynametithi@users.noreply.github.com>
2024-02-06 09:37:55 +0530

    Add files via upload

---
+++ 11_Second_problem.c
@@ -0,0 +1,9 @@
+//Q5)3.0+1 will be :a)integer  b)Floating point number c)charecter
+#include<stdio.h>
+int main(){
+float a=3.0;
+int b=1;
+printf("The value of the number a+b:%f",a+b);
+    return 0;
+}
+//return:The value of the number a+b is :floating point number=4.0000
